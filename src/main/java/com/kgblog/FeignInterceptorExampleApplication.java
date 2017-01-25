4a02995ef45c25124bf06ff5615f352d1dfb5adc
Ersan Ceylan <ersan@kodgemisi.com>
2017-01-25 12:23:58 +0300

    initial commit

---
+++ src/main/java/com/kgblog/FeignInterceptorExampleApplication.java
@@ -0,0 +1,12 @@
+package com.kgblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeignInterceptorExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeignInterceptorExampleApplication.class, args);
+	}
+}
---
+++ src/test/java/com/kgblog/FeignInterceptorExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kgblog;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FeignInterceptorExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
